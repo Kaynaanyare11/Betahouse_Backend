c6fd3dd814e05fad3ef2d551b8ebba8434dced8b
Kaynaanyare11 <khudari.januune.xassan@gmail.com>
2023-07-27 07:39:15 -1200

    cors solution

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const cors = require('cors')
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(cors({Origin: 'http://localhost:5173'}))
 mongoose.set('strictQuery', false)
 mongoose.connect(process.env.DB_URL).then(() => {
   console.log('Connected!')
